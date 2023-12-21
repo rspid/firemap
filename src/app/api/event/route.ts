c87739e6c2e85cd7ba8403f2f31fcb23437bee21
Dav <davidpichard77@gmail.com>
2023-12-21 16:30:20 +0100

    group to endpoint

--- src/app/api/event/route.ts
+++ src/app/api/event/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@/server/db";
 
-//for python gateway
-//create new events
+// for python gateway
+// update sensors intensity and create new events
 export async function POST(request: Request) {
   const res = await request.json();
   const sensors = res.sensors;
@@ -13,60 +13,29 @@ export async function POST(request: Request) {
         },
       });
       if (!oneSensor) return;
+
+      const createNewEvent = sensor.intensity > 0;
+
       const sensorUpdated = await db.sensor.update({
         where: {
           id: sensor.id,
         },
         data: {
           intensity: sensor.intensity,
-          events: {
-            create: [{ event: { create: { is_over: false } } }],
-          },
+          events: createNewEvent
+            ? {
+                create: [{ event: { create: { is_over: false } } }],
+              }
+            : undefined,
         },
         include: {
           events: true,
         },
       });
+
       return sensorUpdated;
     })
   );
 
-  return Response.json({ result });
-}
-
-//for java simulator
-//update sensors intensity
-export async function PUT(request: Request) {
-  const res = await request.json();
-  const sensors = res.sensors;
-  const result = await Promise.all(
-    sensors.map(async (sensor: { id: number; intensity: number }) => {
-      const oneSensor = await db.sensor.update({
-        where: {
-          id: sensor.id,
-        },
-        data: {
-          intensity: sensor.intensity,
-        },
-        include: {
-          events: true,
-        },
-      });
-      if (!oneSensor) return;
-      if (oneSensor.intensity === 0) {
-        oneSensor.events.map((event) => {
-          db.event.update({
-            where: {
-              id: event.event_id,
-            },
-            data: {
-              is_over: true,
-            },
-          });
-        });
-      }
-      return oneSensor;
-    })
-  );
   return Response.json({ result });
 }
