274ed804f7d4fc946daa3057a09bd96d1559449e
Dav <davidpichard77@gmail.com>
2024-01-14 19:10:18 +0100

    fix: return coordinates for steps

--- src/utils/itinerary.ts
+++ src/utils/itinerary.ts
@@ -13,5 +13,19 @@ export async function calculateRouteProperties(start: string, end: string) {
       params,
     }
   );
-  return res.data.features[0].properties;
+
+  const geometry = res.data.features[0].geometry.coordinates;
+  const steps = res.data.features[0].properties.segments[0].steps;
+  const routeSteps = steps.map(
+    (step: { way_points: number[]; duration: number }) => {
+      const coordinates = [];
+
+      for (let i = step.way_points[0]; i <= step.way_points[1]; i++) {
+        coordinates.push(geometry[i]);
+      }
+
+      return { coordinates, duration: step.duration };
+    }
+  );
+  return routeSteps;
 }
