aac43ad187712866a32e1f468fe5d9b967ebfa5d
Dav <davidpichard77@gmail.com>
2024-01-15 17:29:59 +0100

    fix: route condition

--- src/app/api/sensor/noevent/route.ts
+++ src/app/api/sensor/noevent/route.ts
@@ -1,18 +1,69 @@
 import { db } from "@/server/db";
 
+// export async function GET() {
+//   const sensorsWitoutEvents = await db.sensor.findMany({
+//     where: {
+//       intensity: {
+//         not: 0,
+//       },
+//       events: {
+//         every: {
+//           event: {
+//             is_over: true,
+//           },
+//         },
+//       },
+//     },
+//     include: {
+//       events: {
+//         include: {
+//           event: true,
+//         },
+//       },
+//     },
+//   });
+
+//   const sensorsWithoutEvents = await db.sensor.findMany({
+//     where: {
+//       intensity: {
+//         not: 0,
+//       },
+//       events: {
+//         none: {},
+//       },
+//     },
+//   });
+
+//   const allSensors = [...sensorsWitoutEvents, ...sensorsWithoutEvents];
+
+//   return Response.json(allSensors);
+// }
+
 export async function GET() {
-  const sensorsWitoutEvents = await db.sensor.findMany({
+  const sensors = await db.sensor.findMany({
     where: {
-      intensity: {
-        not: 0,
-      },
-      events: {
-        some: {
-          event: {
-            is_over: true,
+      OR: [
+        {
+          intensity: {
+            not: 0,
+          },
+          events: {
+            some: {
+              event: {
+                is_over: true,
+              },
+            },
           },
         },
-      },
+        {
+          intensity: {
+            not: 0,
+          },
+          events: {
+            none: {},
+          },
+        },
+      ],
     },
     include: {
       events: {
@@ -23,18 +74,5 @@ export async function GET() {
     },
   });
 
-  const sensorsWithoutEvents = await db.sensor.findMany({
-    where: {
-      intensity: {
-        not: 0,
-      },
-      events: {
-        none: {},
-      },
-    },
-  });
-
-  const allSensors = [...sensorsWitoutEvents, ...sensorsWithoutEvents];
-
-  return Response.json(allSensors);
+  return Response.json(sensors);
 }
